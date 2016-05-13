a519c98d858195831de2945827e2d2c347913aa0
jomacarpe <jomacarpe@gmail.com>
2016-05-13 20:46:22 +0200

    Cogelo tras comprobar lo de antes

--- prueba/action.c
+++ prueba/action.c
@@ -238,8 +238,8 @@ void printData(fsm_t* this, tmr_t* this1){
 	//printf("tiempo queda = %lld\n", game_time);
 	printf("Rondas = %d\n", round);
 	printf("Fallos = %d\n", fallos);
-	flags=0;
 	delay(anti_rebote+300);
+	flags=0;
 
 
 	  //  fflush(stdout); // Will now print everything in the stout buffer
